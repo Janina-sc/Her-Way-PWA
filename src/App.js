28fdcfb4d2b51b25ce73e2dced24cef65a17c506
Janina Roberto <84505513+Janina-sc@users.noreply.github.com>
2022-03-16 11:44:55 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import mujer from './assets/mujeresunidas.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={mujer} className="App-logo" alt="logo" />
+        <h1>HER WAY</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
